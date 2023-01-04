3996fa3333dd9925d7116721342bc290fca4c011
Stephen Lam <justslamthings@gmail.com>
2023-01-03 19:00:44 -0500

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -7,16 +7,15 @@ function home () {
         <main>
             <h1>REST-Rant</h1>
                             <div>
-                  <img src="images/unsplash.jpg" alt="Unsplash" />
+                  <img src="images/milktea.jpg" alt="Milk Tea" />
                   </div>
       <div>
-        Photo by <a href="https://unsplash.com/@kalvisuals">KAL VISUALS</a> on <a href="https://unsplash.com/photos/aK4iPNYipnU">Unsplash</a>
+        Photo by <a href="https://unsplash.com/@tex450">Matthew Ball</a> on <a href="https://unsplash.com/photos/4HaWpdSoLd0">Unsplash</a>
       </div>
                 
 <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
-
         </main>
     </Def>
   )

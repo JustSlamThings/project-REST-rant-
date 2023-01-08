d59db4f67ca8299ed9d0fb6ca395672fa43c236d
Stephen Lam <justslamthings@gmail.com>
2023-01-08 10:30:58 -0500

    Added the edit functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,9 +6,13 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
-            <h1>{ data.place.city }</h1>
-            <h1>{ data.place.state }</h1>
-            <h1>{ data.place.cuisines }</h1>
+      <img src={data.place.pic} alt={data.place.name} />
+            <h1>City</h1>
+            <h3>{ data.place.city }</h3>
+            <h1>State</h1>
+            <h3>{ data.place.state }</h3>
+            <h1>Cuisines</h1>
+            <h3>{ data.place.cuisines }</h3>
             <h1>Ratings Section</h1>
             <h2>Currently Unrated</h2>
             <h1>Comments Section</h1>
